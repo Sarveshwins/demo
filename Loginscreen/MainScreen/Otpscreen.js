010f7c5170c5773815db1c5e5e84e1d33ed9ac1d
Sarvesh-Awasthi <rajnish16303@gmail.com>
2021-12-07 13:35:36 +0530

    r

--- Loginscreen/MainScreen/Otpscreen.js
+++ Loginscreen/MainScreen/Otpscreen.js
@@ -1,141 +1,208 @@
-import React,{useEffect} from 'react'
-import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native'
-import { connect } from 'react-redux'
-import { bindActionCreators } from 'redux'
-import { attemptUserRegisterActions } from '../actions/UserRegister'
+import React, { useEffect, useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { attemptUserRegisterActions } from "../actions/UserRegister";
 
-const Otpscreen = ({attemptUserRegister ,UserRegisterData}) => {
+const Otpscreen = ({ attemptUserRegister, UserRegisterData }) => {
+  useEffect(() => {}, []);
 
-    useEffect(  () => {
-        attemptUserRegister({
-            email: 'pramod36@softoasistech.com', //getData.username,
-            password: 'ssp@2020', 
-            First_name: "Pradeep",
-            last_name: "Kumar",
-            phone :"9431140126",
-            country_code :"+91",
-            company_name :"Shivam",
-            // getData.password,
-      
-            extraData: async loginRespo => {
-              console.log('loginRespo', loginRespo);
-            //   AppStorage.saveKey(
-            //     key.SAVE_CLIENT_ID,
-            //     JSON.stringify(loginRespo?.user_id),
-            //   );
-            //   navigation.navigate('LoginFF');
+  const [data, setData] = useState({
+    email: "", //pradeep@shivam.com",
+    password: "", //12345678",
+    first_name: "", //"Pradeep",
+    last_name: "", //Kumar",
+    phone: "", // "9431140126",
+    country_code: "", // "+91",
+    company_name: "", // "Shivam",
+  });
+  console.log("dataalert", UserRegisterData);
+  return (
+    <View style={{ flex: 1, justifyContent: "center" }}>
+      <View style={styles.gat}>
+        <TextInput
+          style={{
+            flex: 1,
+          }}
+          // onSubmitEditing={submitHandler}
+          //   textContentType="emailAddress"
+          //   clearButtonMode="always"
+          //   defaultValue={data.username}
+          placeholder=" First-name"
+          //   onChangeText={val => handleUsenameChange(val)}
+          //   keyboardType={'email-address'}
+          //   value={data.username === '' ? '' : data.username}
+          defaultValue={data.first_name}
+          onChangeText={(val) =>
+            setData({
+              ...data,
+              first_name: val,
+            })
+          }
+        />
+      </View>
+
+      <View style={styles.gat}>
+        <TextInput
+          style={{
+            flex: 1,
+          }}
+          placeholder=" Last-name"
+          defaultValue={data.last_name}
+          onChangeText={(val) =>
+            setData({
+              ...data,
+              last_name: val,
+            })
+          }
+        />
+      </View>
+      <View style={styles.gat}>
+        <TextInput
+          style={{
+            flex: 1,
+          }}
+          placeholder=" Company-name"
+          defaultValue={data.company_name}
+          onChangeText={(val) =>
+            setData({
+              ...data,
+              company_name: val,
+            })
+          }
+        />
+      </View>
+      <View style={styles.gat}>
+        <TextInput
+          style={{
+            flex: 1,
+          }}
+          placeholder="Phone"
+          defaultValue={data.phone}
+          onChangeText={(val) =>
+            setData({
+              ...data,
+              phone: val,
+            })
+          }
+        />
+      </View>
+      <View style={styles.gat}>
+        <TextInput
+          style={{
+            flex: 1,
+          }}
+          placeholder="Country-code"
+          defaultValue={data.country_code}
+          onChangeText={(val) =>
+            setData({
+              ...data,
+              country_code: val,
+            })
+          }
+        />
+      </View>
+      <View style={styles.gat}>
+        <TextInput
+          style={{
+            flex: 1,
+          }}
+          placeholder="Email"
+          defaultValue={data.email}
+          onChangeText={(val) =>
+            setData({
+              ...data,
+              email: val,
+            })
+          }
+        />
+      </View>
+      <View style={styles.gat}>
+        <TextInput
+          style={{
+            flex: 1,
+          }}
+          placeholder="Password"
+          defaultValue={data.password}
+          onChangeText={(val) =>
+            setData({
+              ...data,
+              password: val,
+            })
+          }
+        />
+      </View>
+
+      <TouchableOpacity
+        onPress={() =>
+          attemptUserRegister({
+            email: data.email, // "pradeep@shivam.com",
+            password: data.password, // "12345678",
+            first_name: data.first_name, // "Pradeep",
+            last_name: data.last_name, // "Kumar",
+            phone: data.phone, // "9431140126",
+            country_code: data.country_code, // "+91",
+            company_name: data.company_name, // "Shivam",
+
+            extraData: async (loginRespo) => {
+              console.log("loginRespo", loginRespo);
+              //   AppStorage.saveKey(
+              //     key.SAVE_CLIENT_ID,
+              //     JSON.stringify(loginRespo?.user_id),
+              //   );
+              //   navigation.navigate('LoginFF');
             },
-          });
-    }, [])
-    console.log("dataalert",UserRegisterData)
-    return (
-        <View style={{flex:1,justifyContent:"center"}}>
-            <View
-                style={styles.gat}>
-                <TextInput
-                    style={{
-                        flex: 1,
-                    }}
-                    // onSubmitEditing={submitHandler}
-                    //   textContentType="emailAddress"
-                    //   clearButtonMode="always"
-                    //   defaultValue={data.username}
-                    placeholder=" First-name"
-                //   onChangeText={val => handleUsenameChange(val)}
-                //   keyboardType={'email-address'}
-                //   value={data.username === '' ? '' : data.username}
-                />
-            </View>
-
-            <View
-                style={styles.gat}>
-                <TextInput
-                    style={{
-                        flex: 1,
-                    }}
-                    placeholder=" Last-name" />
-            </View>
-            <View
-                style={styles.gat}>
-                <TextInput
-                    style={{
-                        flex: 1,
-                    }}
-                    placeholder=" Company-name" />
-            </View>
-            <View
-                style={styles.gat}>
-                <TextInput
-                    style={{
-                        flex: 1,
-                    }}
-                    placeholder=" Phone" />
-            </View>
-            <View
-                style={styles.gat}>
-                <TextInput
-                    style={{
-                        flex: 1,
-                    }}
-                    placeholder=" Country-code" />
-            </View>
-            <View
-                style={styles.gat}>
-                <TextInput
-                    style={{
-                        flex: 1,
-                    }}
-                    placeholder=" Email" />
-            </View>
-            <View
-                style={styles.gat}>
-                <TextInput
-                    style={{
-                        flex: 1,
-                    }}
-                    placeholder="Password" />
-            </View>
-
-            <TouchableOpacity onPress={() => {}} style={styles.flow}>
-                <Text style={{ fontSize: 20, color: 'white' }}>Submit</Text>
-            </TouchableOpacity>
-        </View>
-    )
-}
+          })
+        }
+        style={styles.flow}
+      >
+        <Text style={{ fontSize: 20, color: "white" }}>Submit</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
 
 // export default Otpscreen
 
 const mapStateToProps = function (state) {
-    return {
-      ...state.UserRegisterReducer,
-    };
+  return {
+    ...state.UserRegisterReducer,
   };
-  export default connect(mapStateToProps, dispatch => ({
-    attemptUserRegister: bindActionCreators(attemptUserRegisterActions.start, dispatch),
-  }))(Otpscreen);
+};
+export default connect(mapStateToProps, (dispatch) => ({
+  attemptUserRegister: bindActionCreators(
+    attemptUserRegisterActions.start,
+    dispatch
+  ),
+}))(Otpscreen);
 
 const styles = StyleSheet.create({
-
-    gat: {
-        width: '80%',
-        height: 40,
-        borderWidth: 0.5,
-        paddingHorizontal: 10,
-        justifyContent: 'center',
-        borderColor: '#9B9B9B',
-        borderRadius: 10,
-        marginTop: 20,
-        alignSelf: "center"
-    },
-    flow: {
-        backgroundColor: '#3d7490',
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginTop: 20,
-        width: '80%',
-        fontSize: 40,
-        borderRadius: 20,
-        height: 40,
-        alignSelf:"center"
-      },
-})
+  gat: {
+    width: "80%",
+    height: 40,
+    borderWidth: 0.5,
+    paddingHorizontal: 10,
+    justifyContent: "center",
+    borderColor: "#9B9B9B",
+    borderRadius: 10,
+    marginTop: 20,
+    alignSelf: "center",
+  },
+  flow: {
+    backgroundColor: "#3d7490",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 20,
+    width: "80%",
+    fontSize: 40,
+    borderRadius: 20,
+    height: 40,
+    alignSelf: "center",
+  },
+});
--- Loginscreen/constants/ApiConst.js
+++ Loginscreen/constants/ApiConst.js
@@ -1,6 +1,6 @@
-export const getLoginPath = () => 'login'; //post
-export const getProfileDetailsPath = () => 'profile_detail'; //post
-export const getMyBookingPath = () => '/my_upcoming_bookings'; //post
-export const getCompletedBookingPath = () => '/my_completed_bookings'; //post
-export const getinprogressBookingPath = () => '/my_underprogress_bookings'; //post
-export const  getRegisterUser =()=> '/register' //post 
+export const getLoginPath = () => "login"; //post
+export const getProfileDetailsPath = () => "profile_detail"; //post
+export const getMyBookingPath = () => "/my_upcoming_bookings"; //post
+export const getCompletedBookingPath = () => "/my_completed_bookings"; //post
+export const getinprogressBookingPath = () => "/my_underprogress_bookings"; //post
+export const getRegisterUser = () => "register"; //post
