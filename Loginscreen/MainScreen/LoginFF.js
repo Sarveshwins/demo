2b951a7ab5cd5c95738132f73e080e45ece25d06
Sarvesh-Awasthi <rajnish16303@gmail.com>
2021-12-08 14:35:52 +0530

    ,

--- Loginscreen/MainScreen/LoginFF.js
+++ Loginscreen/MainScreen/LoginFF.js
@@ -1,51 +1,54 @@
-import React from 'react';
-import {StyleSheet, Text, Image, SafeAreaView, View} from 'react-native';
-import {createDrawerNavigator} from '@react-navigation/drawer';
-import Profilescreen from '../TabsScreen/Profilescreen';
-import Screennotification from '../TabsScreen/Screennotification';
-import Drawercontent from './Drawercontent';
+import React from "react";
+import { StyleSheet, Text, Image, SafeAreaView, View } from "react-native";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import Profilescreen from "../TabsScreen/Profilescreen";
+import Screennotification from "../TabsScreen/Screennotification";
+import Drawercontent from "./Drawercontent";
 
 const Drawer = createDrawerNavigator();
 
 const LoginFF = () => {
   return (
-    <Drawer.Navigator drawerContent={props => <Drawercontent {...props} />}>
+    <Drawer.Navigator drawerContent={(props) => <Drawercontent {...props} />}>
       <Drawer.Screen
         name="profile"
         component={Profilescreen}
         options={{
           title: false,
-          headerBackground: props => (
+          headerBackground: (props) => (
             <SafeAreaView
               style={{
                 flex: 1,
-                backgroundColor: 'white',
-                justifyContent: 'center',
-                alignItems: 'center',
-              }}>
+                backgroundColor: "white",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
               <View
                 style={{
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                  flexDirection: 'row',
+                  justifyContent: "center",
+                  alignItems: "center",
+                  flexDirection: "row",
                   width: 250,
-                }}>
+                }}
+              >
                 <Image
-                  style={{height: 30, width:'100%'}}
-                  source={require('../../assets/header.png')}
+                  style={{ height: 30, width: "100%" }}
+                  source={require("../../assets/header.png")}
                 />
                 <Text
                   style={{
-                    color: 'white',
+                    color: "white",
                     fontSize: 19,
 
                     height: 50,
-                    alignItems: 'center',
-                    alignSelf: 'center',
-                    textAlign: 'center',
+                    alignItems: "center",
+                    alignSelf: "center",
+                    textAlign: "center",
 
-                    fontWeight: 'bold',
-                  }}>
+                    fontWeight: "bold",
+                  }}
+                >
                   {/* CONTICO SHIPPING AGENCY PRIVATE LIMITED */}
                 </Text>
               </View>
