773df480e4384961fbbae046e86d80e637e16196
sahil sharma <89785309+sahilwins@users.noreply.github.com>
2023-04-25 17:33:13 +0530

    up

---
+++ Screen/NewOne.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const NewOne = () => {
+  return (
+    <View>
+      <Text>NewOne</Text>
+    </View>
+  );
+};
+
+export default NewOne;
+
+const styles = StyleSheet.create({});
---
+++ Screen/secondOne.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const secondOne = () => {
+  return (
+    <View>
+      <Text>secondOne</Text>
+    </View>
+  );
+};
+
+export default secondOne;
+
+const styles = StyleSheet.create({});
